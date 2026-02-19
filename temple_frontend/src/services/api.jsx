8e573cc10ab169b806e3081b13a1ca048f51dacb
Anagh04 <anaghop6@gmail.com>
2026-02-19 08:33:58 +0530

    Updated API baseURL to correct backend

--- temple_frontend/src/services/api.jsx
+++ temple_frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://temple-backend.onrender.com/",
+  baseURL: "https://temple-backend.onrender.com/api/",
 });
 
 API.interceptors.request.use((config) => {

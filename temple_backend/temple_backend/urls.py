ea64a3083a029fd41823959a0a37d71087f69a99
Anagh04 <anaghop6@gmail.com>
2026-02-19 09:52:19 +0530

    Add health check route

--- temple_backend/temple_backend/urls.py
+++ temple_backend/temple_backend/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for temple_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    # ✅ Health check route for Render
+    path('', lambda request: HttpResponse("Server Running")),
+
     path('admin/', admin.site.urls),
 
     # 🔥 JWT routes FIRST
@@ -31,5 +19,3 @@ urlpatterns = [
     # 🔥 Then include app routes
     path('api/', include('devotees.urls')),
 ]
-
-

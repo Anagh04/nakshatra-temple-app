6f9c9611a6f74c8fc9a788df7a8808131c604850
Anagh04 <anaghop6@gmail.com>
2026-02-19 13:54:21 +0530

    Updated timezone to Asia/Kolkata

--- temple_backend/temple_backend/settings.py
+++ temple_backend/temple_backend/settings.py
@@ -133,3 +133,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TIME_ZONE = "Asia/Kolkata"
+USE_TZ = True
